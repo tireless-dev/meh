b1f4e60cfbc9990f21022aa21d8e9830ca781ad5
Jerry Shang <jerryshang@gmail.com>
2026-05-30 18:10:11 +0800

    test: group icon snapshot tests by category

---
+++ src/androidHostTest/kotlin/dev/tireless/meh/snapshot/ToolsIconSnapshotTest.kt
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 tireless.dev
+
+package dev.tireless.meh.snapshot
+
+import androidx.compose.ui.test.junit4.v2.createComposeRule
+import androidx.compose.ui.test.onRoot
+import com.github.takahirom.roborazzi.captureRoboImage
+import dev.tireless.meh.icon.preview.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(RobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(sdk = [35], qualifiers = "w800dp-h4000dp-xxhdpi")
+class ToolsIconSnapshotTest {
+
+  @get:Rule
+  val composeTestRule = createComposeRule()
+
+  private fun captureIconPreview(content: @androidx.compose.runtime.Composable () -> Unit) {
+    composeTestRule.setContent {
+      content()
+    }
+    composeTestRule.onRoot().captureRoboImage()
+  }
+
+  @Test fun instruments() = captureIconPreview { IconPackToolsInstrumentsPreview() }
+  @Test fun technology() = captureIconPreview { IconPackToolsTechnologyPreview() }
+}
